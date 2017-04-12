6cafacae874cec106d9eeb6f76eb9e55d97c3641
Sean Bao <baojq@inmindglobal.com>
2017-04-12 14:54:54 +0800

    Wrap root into material ui theme

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import Root from './Root';
 import './main.less';
 
+injectTapEventPlugin();
+
+const App = () => (
+  <MuiThemeProvider>
+    <Root />
+  </MuiThemeProvider>
+);
+
 ReactDOM.render(
-  <Root />,
+  <App />,
   document.getElementById('root'),
 );
