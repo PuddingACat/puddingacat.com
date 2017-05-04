d378d2b8dc58c23e83483f8de3debad2c734b38b
Sean Bao <baojq@inmindglobal.com>
2017-05-04 17:27:08 +0800

    Change navigation buttons behavior for small width screen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,40 +3,83 @@ import { Link } from 'react-router-dom';
 
 import AppBar from 'material-ui/AppBar';
 import FlatButton from 'material-ui/FlatButton';
-import IconMenu from 'material-ui/IconMenu';
+import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import IconButton from 'material-ui/IconButton';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 
-const navButtons = (
-  <div>
-    <nav className="appbar-nav-buttons-container">
-      <FlatButton
-        label="HOME"
-        labelStyle={{ color: '#fff' }}
-        containerElement={<Link to="/" />} />
-      <FlatButton
-        label="ABOUT"
-        labelStyle={{ color: '#fff' }}
-        containerElement={<Link to="/about" />} />
-    </nav>
-    <IconMenu
-      className="appbar-nav-icon-menu"
-      iconButtonElement={<IconButton><MoreVertIcon color="#fff" /></IconButton>}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      targetOrigin={{ vertical: 'top', horizontal: 'right' }} >
-      <MenuItem primaryText="HOME" />
-      <MenuItem primaryText="ABOUT" />
-    </IconMenu>
-  </div>
-);
+class NavActions extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      drawerOpen: false,
+    };
+  }
+
+  openNavDrawer = () => {
+    this.setState({
+      drawerOpen: true,
+    });
+  }
+
+  closeNavDrawer = () => {
+    this.setState({
+      drawerOpen: false,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="appbar-nav-buttons-container">
+          <FlatButton
+            label="HOME"
+            labelStyle={{ color: '#fff' }}
+            containerElement={<Link to="/" />} />
+          <FlatButton
+            label="ABOUT"
+            labelStyle={{ color: '#fff' }}
+            containerElement={<Link to="/about" />} />
+        </nav>
+
+        <IconButton
+          className="appbar-nav-drawer-button"
+          onTouchTap={this.openNavDrawer}>
+          <MoreVertIcon color="#fff" />
+        </IconButton>
+        <Drawer
+          className="appbar-nav-drawer"
+          disableSwipeToOpen={true}
+          width="60%"
+          docked={false}
+          openSecondary={true}
+          open={this.state.drawerOpen}
+          onRequestChange={drawerOpen => this.setState({ drawerOpen })} >
+          <AppBar showMenuIconButton={false} />
+          <MenuItem
+            className="appbar-nav-drawer-item"
+            onTouchTap={this.closeNavDrawer}
+            containerElement={<Link to="/" />} >
+            HOME
+          </MenuItem>
+          <MenuItem
+            className="appbar-nav-drawer-item"
+            onTouchTap={this.closeNavDrawer}
+            containerElement={<Link to="/about" />} >
+            ABOUT
+          </MenuItem>
+        </Drawer>
+      </div>
+    );
+  }
+}
 
 const Header = props => (
   <AppBar
     className="page-header"
     showMenuIconButton={false}
     title="Pudding A Cat"
-    iconElementRight={navButtons} />
+    iconElementRight={<NavActions />} />
 );
 
 export default Header;
